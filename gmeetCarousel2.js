8445f3acc41fb82f2bb64b8951ca4292f6f8d11a
jmutolo <102764323+jmutolo@users.noreply.github.com>
2023-04-25 14:19:23 -0300

    Update and rename gmeetCarousel.js to gmeetCarousel2.js

---
+++ gmeetCarousel2.js
@@ -0,0 +1,32 @@
+document.getElementById('gmeetCarousel').innerHTML = `
+<section class="splide" aria-label="Splide Basic HTML Example">
+<div class="splide__track">
+      <ul class="splide__list">
+            <li class="splide__slide">
+                <div class="cardwrapper">
+                    <div class="coursecardbghover"></div>
+                    <div class="coursecardbg"></div>
+                    <a data-w-id="4d92e966-a4b5-f956-fda1-1d3a2beb4256" href="#" class="coursecardcontent w-inline-block">
+                        <div class="div-block-57">
+                            <div class="filterbutton">
+                                <div>Day 1</div>
+                            </div>
+                            <div class="div-block-58"><img src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/6447e8cd1f6efe5846d137b6_Group%20(15).svg" loading="lazy" alt="" class="image-54" /></div>
+                        </div>
+                        <div class="div-block-59">
+                            <div class="div-block-60">
+                                <div class="text-block-51">
+                                    Meeting<br />
+                                    Take off
+                                </div>
+                                <div class="textheadermain">Start proyect meeting with the client</div>
+                            </div>
+                        </div>
+                    </a>
+                    <img src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/6447ebf7b129676ea0f59691_Flow.svg" loading="lazy" alt="" class="image-55" />
+                </div>
+            </li>
+      </ul>
+</div>
+</section>
+`;
