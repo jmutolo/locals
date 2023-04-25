26ea15ffca100d3957dfcaa177decc31f9eaff29
jmutolo <102764323+jmutolo@users.noreply.github.com>
2023-04-25 14:33:28 -0300

    Update and rename gmeetCarousel2.js to gmeetCarousel3.js

---
+++ gmeetCarousel3.js
@@ -0,0 +1,245 @@
+document.getElementById('gmeetCarousel').innerHTML = `
+<section class="splide" aria-label="Splide Basic HTML Example">
+<div class="splide__track">
+      <ul class="splide__list">
+            <li class="splide__slide">
+                <div class="cardwrapper">
+                    <div class="coursecardbghover"></div>
+                    <div class="coursecardbg"></div>
+                    <a data-w-id="4d92e966-a4b5-f956-fda1-1d3a2beb4256" href="#" class="coursecardcontent w-inline-block">
+                        <div class="div-block-57">
+                            <div class="filterbutton">
+                                <div>Day 1</div>
+                            </div>
+                            <div class="div-block-58"><img src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/6447e8cd1f6efe5846d137b6_Group%20(15).svg" loading="lazy" alt="" class="image-54" /></div>
+                        </div>
+                        <div class="div-block-59">
+                            <div class="div-block-60">
+                                <div class="text-block-51">
+                                    Meeting<br />
+                                    Take off
+                                </div>
+                                <div class="textheadermain">Start proyect meeting with the client</div>
+                            </div>
+                        </div>
+                    </a>
+                    <img src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/6447ebf7b129676ea0f59691_Flow.svg" loading="lazy" alt="" class="image-55" />
+                </div>
+            </li>
+            <li class="splide__slide">
+                <div class="cardwrapper">
+                    <div class="coursecardbghover"></div>
+                    <div class="coursecardbg"></div>
+                    <a data-w-id="ff2421e5-6422-9525-eabb-210f8a2f5a0f" href="#" class="coursecardcontent w-inline-block">
+                        <div class="div-block-57">
+                            <div class="filterbutton">
+                                <div>Day 2</div>
+                            </div>
+                            <div class="div-block-58"><img src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/6447e8cd1f6efe5846d137b6_Group%20(15).svg" loading="lazy" alt="" class="image-54" /></div>
+                        </div>
+                        <div class="div-block-59">
+                            <div class="div-block-60">
+                                <div class="text-block-51">
+                                    Assume as<br />
+                                    a begginer
+                                </div>
+                                <div class="textheadermain">Start proyect meeting with the client</div>
+                            </div>
+                        </div>
+                    </a>
+                    <img src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/6447ebf7b129676ea0f59691_Flow.svg" loading="lazy" alt="" class="image-55" />
+                </div>
+            </li>
+            <li class="splide__slide">
+                <div class="cardwrapper">
+                    <div class="coursecardbghover"></div>
+                    <div class="coursecardbg"></div>
+                    <a data-w-id="f313c83a-0bfe-f0b1-fcb8-28182d3a60e9" href="#" class="coursecardcontent w-inline-block">
+                        <div class="div-block-57">
+                            <div class="filterbutton">
+                                <div>Day 3</div>
+                            </div>
+                            <div class="div-block-58"><img src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/6447e8cd1f6efe5846d137b6_Group%20(15).svg" loading="lazy" alt="" class="image-54" /></div>
+                        </div>
+                        <div class="div-block-59">
+                            <div class="div-block-60">
+                                <div class="text-block-51">
+                                    Start<br />
+                                    research
+                                </div>
+                                <div class="textheadermain">Start proyect meeting with the client</div>
+                            </div>
+                        </div>
+                    </a>
+                    <img src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/6447ebf7b129676ea0f59691_Flow.svg" loading="lazy" alt="" class="image-55" />
+                </div>
+            </li>
+            <li class="splide__slide">
+                <div class="cardwrapper">
+                    <div class="coursecardbghover"></div>
+                    <div class="coursecardbg"></div>
+                    <a data-w-id="1f2b933d-55bf-88c0-1ee1-5db6ae2f8f13" href="#" class="coursecardcontent w-inline-block">
+                        <div class="div-block-57">
+                            <div class="filterbutton">
+                                <div>Day 4</div>
+                            </div>
+                            <div class="div-block-58"><img src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/6447e8cd1f6efe5846d137b6_Group%20(15).svg" loading="lazy" alt="" class="image-54" /></div>
+                        </div>
+                        <div class="div-block-59">
+                            <div class="div-block-60">
+                                <div class="text-block-51">Moodboard</div>
+                                <div class="textheadermain">Start proyect meeting with the client</div>
+                            </div>
+                        </div>
+                    </a>
+                    <img src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/6447ebf7b129676ea0f59691_Flow.svg" loading="lazy" alt="" class="image-55" />
+                </div>
+            </li>
+            <li class="splide__slide">
+                <div class="cardwrapper">
+                    <div class="coursecardbghover"></div>
+                    <div class="coursecardbg"></div>
+                    <a data-w-id="806bb615-004d-8763-b3af-1b0b64879f63" href="#" class="coursecardcontent w-inline-block">
+                        <div class="div-block-57">
+                            <div class="filterbutton">
+                                <div>Day 5 &amp; 6</div>
+                            </div>
+                            <div class="div-block-58 _58-2"><img src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/6447e8cd1f6efe5846d137b6_Group%20(15).svg" loading="lazy" alt="" class="image-54" /></div>
+                        </div>
+                        <div class="div-block-59">
+                            <div class="div-block-60">
+                                <div class="text-block-51">
+                                    Start<br />
+                                    sketching
+                                </div>
+                                <div class="textheadermain">Start proyect meeting with the client</div>
+                            </div>
+                        </div>
+                    </a>
+                    <img src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/6447ebf7b129676ea0f59691_Flow.svg" loading="lazy" alt="" class="image-55" />
+                </div>
+            </li>
+            <li class="splide__slide">
+                <div class="cardwrapper">
+                    <div class="coursecardbghover"></div>
+                    <div class="coursecardbg"></div>
+                    <a data-w-id="f297308e-bc34-0095-87c7-f68894f88369" href="#" class="coursecardcontent w-inline-block">
+                        <div class="div-block-57">
+                            <div class="filterbutton">
+                                <div>Day 7</div>
+                            </div>
+                            <div class="div-block-58"><img src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/6447e8cd1f6efe5846d137b6_Group%20(15).svg" loading="lazy" alt="" class="image-54" /></div>
+                        </div>
+                        <div class="div-block-59">
+                            <div class="div-block-60">
+                                <div class="text-block-51">
+                                    Meeting<br />
+                                    presentation
+                                </div>
+                                <div class="textheadermain">Start proyect meeting with the client</div>
+                            </div>
+                        </div>
+                    </a>
+                    <img src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/6447ebf7b129676ea0f59691_Flow.svg" loading="lazy" alt="" class="image-55" />
+                </div>
+            </li>
+            <li class="splide__slide">
+                <div class="cardwrapper">
+                    <div class="coursecardbghover"></div>
+                    <div class="coursecardbg"></div>
+                    <a data-w-id="d8b39d1d-324b-b4a2-ef74-d845ff758413" href="#" class="coursecardcontent w-inline-block">
+                        <div class="div-block-57">
+                            <div class="filterbutton">
+                                <div>Day 8 &amp; 9</div>
+                            </div>
+                            <div class="div-block-58 _58-2"><img src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/6447e8cd1f6efe5846d137b6_Group%20(15).svg" loading="lazy" alt="" class="image-54" /></div>
+                        </div>
+                        <div class="div-block-59">
+                            <div class="div-block-60">
+                                <div class="text-block-51">
+                                    <br />
+                                    Rework
+                                </div>
+                                <div class="textheadermain">Start proyect meeting with the client</div>
+                            </div>
+                        </div>
+                    </a>
+                    <img src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/6447ebf7b129676ea0f59691_Flow.svg" loading="lazy" alt="" class="image-55" />
+                </div>
+            </li>
+            <li class="splide__slide">
+                <div class="cardwrapper">
+                    <div class="coursecardbghover"></div>
+                    <div class="coursecardbg"></div>
+                    <a data-w-id="1ae4f2e9-3d70-cac7-e7a5-c375a870ce3c" href="#" class="coursecardcontent w-inline-block">
+                        <div class="div-block-57">
+                            <div class="filterbutton">
+                                <div>Day 10</div>
+                            </div>
+                            <div class="div-block-58"><img src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/6447e8cd1f6efe5846d137b6_Group%20(15).svg" loading="lazy" alt="" class="image-54" /></div>
+                        </div>
+                        <div class="div-block-59">
+                            <div class="div-block-60">
+                                <div class="text-block-51">
+                                    Creative<br />
+                                    profile
+                                </div>
+                                <div class="textheadermain">Start proyect meeting with the client</div>
+                            </div>
+                        </div>
+                    </a>
+                    <img src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/6447ebf7b129676ea0f59691_Flow.svg" loading="lazy" alt="" class="image-55" />
+                </div>
+            </li>
+            <li class="splide__slide">
+                <div class="cardwrapper">
+                    <div class="coursecardbghover"></div>
+                    <div class="coursecardbg"></div>
+                    <a data-w-id="1698f148-b3c9-960b-4b22-0e3775255658" href="#" class="coursecardcontent w-inline-block">
+                        <div class="div-block-57">
+                            <div class="filterbutton">
+                                <div>Day 11</div>
+                            </div>
+                            <div class="div-block-58"><img src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/6447e8cd1f6efe5846d137b6_Group%20(15).svg" loading="lazy" alt="" class="image-54" /></div>
+                        </div>
+                        <div class="div-block-59">
+                            <div class="div-block-60">
+                                <div class="text-block-51">
+                                    Final<br />
+                                    adjustment
+                                </div>
+                                <div class="textheadermain">Start proyect meeting with the client</div>
+                            </div>
+                        </div>
+                    </a>
+                    <img src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/6447ebf7b129676ea0f59691_Flow.svg" loading="lazy" alt="" class="image-55" />
+                </div>
+            </li>
+            <li class="splide__slide">
+                <div class="cardwrapper">
+                    <div class="coursecardbghover"></div>
+                    <div class="coursecardbg"></div>
+                    <a data-w-id="e240a595-e88b-fc33-f9f5-b49ed3af2df6" href="#" class="coursecardcontent w-inline-block">
+                        <div class="div-block-57">
+                            <div class="filterbutton">
+                                <div>Day 12</div>
+                            </div>
+                            <div class="div-block-58"><img src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/6447e8cd1f6efe5846d137b6_Group%20(15).svg" loading="lazy" alt="" class="image-54" /></div>
+                        </div>
+                        <div class="div-block-59">
+                            <div class="div-block-60">
+                                <div class="text-block-51">
+                                    Meeting<br />
+                                    presentation
+                                </div>
+                                <div class="textheadermain">Start proyect meeting with the client</div>
+                            </div>
+                        </div>
+                    </a>
+                    <img src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/6447ebf7b129676ea0f59691_Flow.svg" loading="lazy" alt="" class="image-55" />
+                </div>
+            </li>
+      </ul>
+</div>
+</section>
+`;
