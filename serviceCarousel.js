ebbc0d72c9b714f908a0ba722db4ffd32068a205
jmutolo <102764323+jmutolo@users.noreply.github.com>
2023-12-27 20:01:46 -0300

    Add files via upload

---
+++ serviceCarousel.js
@@ -0,0 +1,161 @@
+document.getElementById('serviceCarousel').innerHTML = `
+
+
+<section id="brand-services" class="splide" aria-label="Recent Works Slider">
+
+    <div class="splide__track">
+
+        <ul class="splide__list">
+
+            <li class="splide__slide">
+
+
+                <div class="brandservice">
+                    <img
+                        src="https://assets-global.website-files.com/642c316d40dc3f5a58b050d7/658c96f431b46a0415ef1349_serviceBackground1.webp"
+                        loading="lazy"
+                        sizes="(max-width: 1728px) 90vw, 1556px"
+                        srcset="
+                            https://assets-global.website-files.com/642c316d40dc3f5a58b050d7/658c96f431b46a0415ef1349_serviceBackground1-p-500.webp   500w,
+                            https://assets-global.website-files.com/642c316d40dc3f5a58b050d7/658c96f431b46a0415ef1349_serviceBackground1-p-800.webp   800w,
+                            https://assets-global.website-files.com/642c316d40dc3f5a58b050d7/658c96f431b46a0415ef1349_serviceBackground1-p-1080.webp 1080w,
+                            https://assets-global.website-files.com/642c316d40dc3f5a58b050d7/658c96f431b46a0415ef1349_serviceBackground1.webp        1556w
+                        "
+                        alt=""
+                        class="bgimagebrandservice"
+                    />
+                    <div class="detailscontainer">
+                        <div class="detailheader">Branding</div>
+                        <div class="detailtitle">
+                            The power of <br />
+                            how it feels,<br />
+                            thats branding
+                        </div>
+                        <div class="buttonrow">
+                            <a href="#" class="servicebutton w-inline-block">
+                                <div>Strategy</div>
+                            </a>
+                            <a href="#" class="servicebutton w-inline-block">
+                                <div>Planning</div>
+                            </a>
+                            <a href="#" class="servicebutton w-inline-block">
+                                <div>Customer profile</div>
+                            </a>
+                            <a href="#" class="servicebutton w-inline-block">
+                                <div>Values</div>
+                            </a>
+                            <a href="#" class="servicebutton w-inline-block">
+                                <div>Mision</div>
+                            </a>
+                            <a href="#" class="servicebutton w-inline-block">
+                                <div>Visión</div>
+                            </a>
+                        </div>
+                    </div>
+                    <a href="#" class="bottombuttonwrapper w-inline-block">
+                        <div class="detailheader">Open the service</div>
+                        <img src="https://assets-global.website-files.com/642c316d40dc3f5a58b050d7/658c965ddab225dd6b2dcd73_Vector%20568.svg" loading="lazy" alt="" class="image-112" />
+                    </a>
+                </div>
+                
+
+            </li>
+
+            <li class="splide__slide">
+
+
+                <div class="brandservice">
+                    <img
+                        src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/658cab11cc046e56b4dc7bd3_serviceBackground2.webp"
+                        loading="lazy"
+                        sizes="(max-width: 1728px) 90vw, 1556px"
+                        srcset="
+                            https://assets-global.website-files.com/642c316d40dc3f5a58b050d7/658c96f431b46a0415ef1349_serviceBackground1-p-500.webp   500w,
+                            https://assets-global.website-files.com/642c316d40dc3f5a58b050d7/658c96f431b46a0415ef1349_serviceBackground1-p-800.webp   800w,
+                            https://assets-global.website-files.com/642c316d40dc3f5a58b050d7/658c96f431b46a0415ef1349_serviceBackground1-p-1080.webp 1080w,
+                            https://assets-global.website-files.com/642c316d40dc3f5a58b050d7/658c96f431b46a0415ef1349_serviceBackground1.webp        1556w
+                        "
+                        alt=""
+                        class="bgimagebrandservice"
+                    />
+                    <div class="detailscontainer">
+                        <div class="detailheader">Brand Identity</div>
+                        <div class="detailtitle">
+                            The power of <br />
+                            a simple form,<br />
+                            thats identity
+                        </div>
+                        <div class="buttonrow">
+                            <a href="#" class="servicebutton w-inline-block">
+                                <div>Logo</div>
+                            </a>
+                            <a href="#" class="servicebutton w-inline-block">
+                                <div>Brand</div>
+                            </a>
+                        </div>
+                    </div>
+                    <a href="#" class="bottombuttonwrapper w-inline-block">
+                        <div class="detailheader">Open the service</div>
+                        <img src="https://assets-global.website-files.com/642c316d40dc3f5a58b050d7/658c965ddab225dd6b2dcd73_Vector%20568.svg" loading="lazy" alt="" class="image-112" />
+                    </a>
+                </div>
+                
+
+            </li>
+
+            <li class="splide__slide">
+
+
+                <div class="brandservice">
+                    <img
+                        src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/658cab1114bd0e7046cb68e6_serviceBackground3.webp"
+                        loading="lazy"
+                        sizes="(max-width: 1728px) 90vw, 1556px"
+                        srcset="
+                            https://assets-global.website-files.com/642c316d40dc3f5a58b050d7/658c96f431b46a0415ef1349_serviceBackground1-p-500.webp   500w,
+                            https://assets-global.website-files.com/642c316d40dc3f5a58b050d7/658c96f431b46a0415ef1349_serviceBackground1-p-800.webp   800w,
+                            https://assets-global.website-files.com/642c316d40dc3f5a58b050d7/658c96f431b46a0415ef1349_serviceBackground1-p-1080.webp 1080w,
+                            https://assets-global.website-files.com/642c316d40dc3f5a58b050d7/658c96f431b46a0415ef1349_serviceBackground1.webp        1556w
+                        "
+                        alt=""
+                        class="bgimagebrandservice"
+                    />
+                    <div class="detailscontainer">
+                        <div class="detailheader">Digital products</div>
+                        <div class="detailtitle">
+                            The power of <br />
+                            how it feels,<br />
+                            thats branding
+                        </div>
+                        <div class="buttonrow">
+                            <a href="#" class="servicebutton w-inline-block">
+                                <div>Website</div>
+                            </a>
+                            <a href="#" class="servicebutton w-inline-block">
+                                <div>Ecommerce</div>
+                            </a>
+                            <a href="#" class="servicebutton w-inline-block">
+                                <div>Platforms</div>
+                            </a>
+                            <a href="#" class="servicebutton w-inline-block">
+                                <div>Apps</div>
+                            </a>
+                        </div>
+                    </div>
+                    <a href="#" class="bottombuttonwrapper w-inline-block">
+                        <div class="detailheader">Open the service</div>
+                        <img src="https://assets-global.website-files.com/642c316d40dc3f5a58b050d7/658c965ddab225dd6b2dcd73_Vector%20568.svg" loading="lazy" alt="" class="image-112" />
+                    </a>
+                </div>
+                
+
+            </li>
+
+        </ul>
+
+    </div>
+
+
+</section>
+
+`;
