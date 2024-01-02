c47802dc2b7cad9d5970f9ddfe641931f531f34e
jmutolo <102764323+jmutolo@users.noreply.github.com>
2024-01-01 23:55:11 -0300

    Add files via upload

---
+++ latestWorksCarousel.js
@@ -0,0 +1,79 @@
+
+document.getElementById('latest-works').innerHTML = `
+
+
+<section id="latest-works-carousel" class="splide" aria-label="Latest Works Slider">
+
+    <div class="splide__track">
+
+        <ul class="splide__list">
+
+            <li class="splide__slide">
+
+                <img
+                        src="https://assets-global.website-files.com/642c316d40dc3f5a58b050d7/658cfb3cbf5859f359157c99_Group%20427324098.webp"
+                        loading="lazy" alt="" class="latestworksimage" 
+                        
+                />
+
+            </li>
+
+            <li class="splide__slide">
+
+                <img
+                        src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/658cfb45b9114f8fc4ec19f5_Recurso%202%402x-100%20(1).webp"
+                        loading="lazy" alt="" class="latestworksimage" 
+                        
+                />
+
+            </li>
+
+            <li class="splide__slide">
+
+                <img
+                        src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/658cfb4cb700e73390c1a4e7_Frame%20427323712.webp"
+                        loading="lazy" alt="" class="latestworksimage" 
+                        
+                />
+
+            </li>
+
+            <li class="splide__slide">
+
+                <img
+                        src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/658cfb522f16f6d662dcf9c0_Frame%20427323711.webp"
+                        loading="lazy" alt="" class="latestworksimage" 
+                        
+                />
+
+            </li>
+
+            <li class="splide__slide">
+
+                <img
+                        src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/658cfb5a822af522de23b3dd_Frame%20427323713.webp"
+                        loading="lazy" alt="" class="latestworksimage" 
+                        
+                />
+
+            </li>
+
+            <li class="splide__slide">
+
+                <img
+                        src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/658cfb6197589af28fb49e10_Frame%20427323714.webp"
+                        loading="lazy" alt="" class="latestworksimage" 
+                        
+                />
+
+            </li>
+
+        </ul>
+
+
+    </div>
+
+
+</section>
+
+`;
