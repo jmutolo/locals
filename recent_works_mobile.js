5a025853ba97851354ed2b910a384049b21ed0c1
jmutolo <joaquinmutolo@gmail.com>
2024-04-01 19:10:08 -0300

    recent works mobile

---
+++ recent_works_mobile.js
@@ -0,0 +1,231 @@
+document.getElementById('recent_works_mobile').innerHTML = `
+<section id="recent-works" class="splide" aria-label="Recent Works Slider">
+    <div class="splide__track">
+            <ul class="splide__list">
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b1233eb4efef74349d689_01.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d04ace31773f2aa00e_05.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12334ace31773f2a1bfe_02.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d52e04e6c6b117dc6e_58.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b122ffb6aaed0f3b405a5_03.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d14ad590770d007bc0_10.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b122f50f078b14d11ea06_04.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d11eefd2b26ac27de5_17.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b122f17a89970001d1b3b_05.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d5431d446d62975fa7_65.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b122f02f3688a44a676f9_06.svg"/>
+                    <img class="fullbrandmobile" src="#"/>
+                </li>
+                <!---->
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b122fed550be5a054e2ee_07.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d68a935fd1a5091455_67.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b122e22ca975d486f23d2_08.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d1a4d6ef180f5c09b8_09.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b122eb23408ae32a072ec_09.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d682e7091d0491364f_68.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b122f30746cf4170d6601_10.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d144285ce0d6212396_12.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b122f34ad981cbcd2fca8_11.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d1c457ed880cb1117a_06.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b122e1a0b6436fecb9b7f_12.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d10ddf8f08c8dac86b_08.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b122f30746cf4170d65fa_13.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d110d30018c4bcd1ed_11.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b122eff88d052d61752a1_14.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d127d524fbd02bdb42_15.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b122f76e2af75783cf4a4_15.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d522ca975d486fa32b_55.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b122fb8f4f3b070955c1e_16.svg"/>
+                    <img class="fullbrandmobile" src="#"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b122f1509fe438aae86c0_17.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d617a89970001da625_69.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b122fdb2057c351014679_18.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d64f5069adfca0c25e_76.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b122f1509fe438aae86c5_19.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d310d30018c4bcd3e8_37.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b122fc457ed880cb0724e_20.svg"/>
+                    <img class="fullbrandmobile" src="#"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b122f4ad590770dffcd69_21.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d26116b547e354d21d_26.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b1230f76d28a05788ed0f_22.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d20d2ea4180e70aa54_19.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b122f76e2af75783cf4e9_23.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d34f5069adfca0c116_38.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b122feb4efef74349d49f_24.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d1431d446d62975d4a_16.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b123037230936e33e08f5_25.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d65a222d6a2b517739_72.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b122fbe54f454d7e75868_26.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d66ca9619f7a8b6a3e_73.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b123076e2af75783cf515_27.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d41eefd2b26ac2813b_47.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b1231f76d28a05788ee56_28.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d6f76d28a057897e0e_71.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12304f5069adfca003f2_29.svg"/>
+                    <img class="fullbrandmobile" src="#"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b1230eb4efef74349d501_30.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d61eefd2b26ac28524_70.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b1231fb6aaed0f3b40903_31.svg"/>
+                    <img class="fullbrandmobile" src="#"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b1231f0f9891adf860f87_32.svg"/>
+                    <img class="fullbrandmobile" src="#"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b123182e7091d049086ac_33.svg"/>
+                    <img class="fullbrandmobile" src="#"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12301ecf58004438b7ee_34.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d116c50dc1d6253b0b_04.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12313d1dde5defefc713_35.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d010d30018c4bcd10d_03.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b1230eb4efef74349d522_36.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d444285ce0d62125a6_45.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12301a0b6436fecb9e37_37.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d16c97041d2e56eba7_07.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b1231eb4efef74349d54f_38.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d75a222d6a2b5177a3_79.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b1232e1d5f82d2199dabe_39.svg"/>
+                    <img class="fullbrandmobile" src="#"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b1232ed550be5a054e7d9_40.svg"/>
+                    <img class="fullbrandmobile" src="#"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b123176e2af75783cf6f8_41.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d309facb9ea1362223_36.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b1232dbd56c6027238eb2_42.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d6eb4efef7434a5de9_66.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12325a9bd1bd3ce969d7_43.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d26116b547e354d227_27.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b1232094eb5761875a268_44.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d16987d7cb6f6bdff0_14.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b1232e33506bfc884611c_45.svg"/>
+                    <img class="fullbrandmobile" src="#"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12320f67f5a3e0bcf011_46.svg"/>
+                    <img class="fullbrandmobile" src="#"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12324f5069adfca004b3_47.svg"/>
+                    <img class="fullbrandmobile" src="#"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b123253abd045a46fc2d6_48.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d1eb4efef7434a5a29_13.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b1232a4d6ef180f5b6c53_49.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d14568f2b5b173f8b4_83.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b123217a89970001d1d71_50.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d017a89970001da25f_86.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b1232d2107bf5012bf389_51.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d6eb4efef7434a5def_75.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12334ace31773f2a1b06_52.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d176e2af75783d798a_82.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12334f5069adfca005b5_53.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d16987d7cb6f6bdf80_01.svg"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b1233b8f4f3b070955e9f_54.svg"/>
+                    <img class="fullbrandmobile" src="#"/>
+                </li>
+                <li class="splide__slide">
+                    <img class="brandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12331a0b6436fecb9fd2_55.svg"/>
+                    <img class="fullbrandmobile" src="https://uploads-ssl.webflow.com/642c316d40dc3f5a58b050d7/660b12d250f078b14d126c13_21.svg"/>
+                </li>
+
+                
+            </ul>
+    </div>
+</section>
+`;
